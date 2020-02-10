e30be1e915dd7d67fdf0d6278d429dd63480c65a
AJ Guiles <ashiguiles@gmail.com>
2020-02-10 19:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 2 
+const num2 = 31
 
-// Write your code here
+let multiply = num2 * num2
+let random = Math.floor(Math.random() * 10) + 1
+let mod = 24 % 5
+let max = Math.max(2,3,4,5,20)
+
+
+
